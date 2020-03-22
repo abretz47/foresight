5a8e76dd91aa7452403c39323671326f0fe4cab4
abretz47 <abretz47@gmail.com>
2020-03-22 06:30:43 -0500

    login working

--- App.js
+++ App.js
@@ -12,24 +12,26 @@ import * as DB from './src/data/db';
 
 async function logIn() {
   try {
-    const {
-      type,
-      token,
-      expires,
-      permissions,
-      declinedPermissions,
-    } = await Facebook.logInWithReadPermissionsAsync('', {
-      permissions: ['public_profile'],
-      behavior: 'browser'
-    });
-    if (type === 'success') {
-      // Get the user's name using Facebook's Graph API
-      const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-      Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
-    } else {
-      // type === 'cancel'
-      alert('something else');
-    }
+    await Facebook.initializeAsync('2668752273380739', "foresight");
+      const {
+        type,
+        token,
+        expires,
+        permissions,
+        declinedPermissions,
+      } = await Facebook.logInWithReadPermissionsAsync('2668752273380739', {
+        permissions: ['public_profile'],
+        behavior: 'browser'
+      });
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`)
+          Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+      } else {
+        // type === 'cancel'
+        alert('something else');
+      }
+    
   } catch ({ message }) {
     alert(`Facebook Login Error: ${message}`);
   }
